7a1c0b6b9b47bb58d4d1229d8f513eda3078266b
SujayPrabhu96 <sujay@codemancers.com>
2021-09-11 18:55:04 +0530

    Added posts controller and render static content using variables from controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+    def index
+        @content_first = 'This is my first post from Ruby blog'
+        @content_second = 'This is my second post from Ruby blog'
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def show
+
+    end
+
+    def destory
+
+    end
+end
