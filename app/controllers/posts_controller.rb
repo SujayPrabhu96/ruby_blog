651ddc6d20318aa7f14ef0b943c9e8d2292b64ca
SujayPrabhu96 <prabhusujay@gmail.com>
2021-09-23 23:37:28 +0530

    CRUD operation with Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,22 +8,40 @@ class PostsController < ApplicationController
     end
 
     def new
-
+        @post = Post.new
+        @categories = Category.all
     end
 
     def create
-
+        @post = Post.new(permit_params)
+        if @post.save
+            redirect_to posts_path, :notice => "Post saved Successfully!"
+        else
+            render "new"
+        end
     end
 
     def edit
-
+        @post = Post.find(params[:id])
+        @categories = Category.all
     end
 
     def update
-
+        @post = Post.find(params[:id])
+        if @post.update(permit_params)
+            redirect_to post_path, :notice => "Post updated Successfully!"
+        else
+            render "edit"
+        end
     end
 
-    def destory
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path, :notice => "Post deleted Successfully!"
+    end
 
+    def permit_params
+        params.require(:post).permit(:title, :body, :category_id)
     end
 end
